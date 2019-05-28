a4c2fd0af402eb2f1bf44a2fbde016300c19003c
MangoMaster <weijd17@mails.tsinghua.edu.cn>
2019-05-28 20:51:02 +0800

    Add word-embedding representation of texts

--- src/bake_data.py
+++ src/bake_data.py
@@ -3,6 +3,7 @@ import re
 import pickle
 import numpy as np
 from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
 
 def collect_data(data_file_name, output_file_name_suffix):
@@ -101,7 +102,7 @@ def tf_idf():
     """
     Convert text data to tf-idf, using np to save.
     w(i)(j) = tf(i)(j) * idf(j) 表示第i个文档中第j个词的tf-idf值
-    tf(i)(j) = 1 + np.log(count(i)(j)) 
+    tf(i)(j) = 1 + np.log(count(i)(j))
     count(i)(j)表示第i个文档中第j个词出现的次数
     idf(j) = log(1 + document_count / (1 + df(j)))
     df(j)表示出现第j个词的文档数
@@ -134,6 +135,71 @@ def tf_idf():
     print(matrix_test.shape)
     with open(output_test_file_path, 'wb') as output_file:
         np.save(output_file, matrix_test)
+
+
+def word_embedding(max_sequence_length=300):
+    """
+    Convert text data to word-embedding, using np to save.
+    w(i)(j)表示第i个文档中第j个词在word_embedding词表中对应的index.
+    Convert word2vec data to word-embedding matrix, using np to save.
+    只保留在训练语料库中出现的词向量，组成word-embedding matrix.
+    @param: max_sequence_length: used to pad index sequence of a text.
+    """
+    # Generate file path
+    texts_train_file_path = os.path.join(os.path.pardir, "data", "texts_train")
+    output_train_file_path = os.path.join(
+        os.path.pardir, "data", "word-embedding_train")
+    texts_test_file_path = os.path.join(os.path.pardir, "data", "texts_test")
+    output_test_file_path = os.path.join(
+        os.path.pardir, "data", "word-embedding_test")
+    # Convert train text to word-embedding
+    with open(texts_train_file_path, 'rb') as texts_train_file:
+        texts_train = pickle.load(texts_train_file)
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(texts_train)
+    sequences_train = tokenizer.texts_to_sequences(texts_train)
+    sequences_train = pad_sequences(
+        sequences_train, maxlen=max_sequence_length)
+    # Save train sequences
+    print(len(tokenizer.word_index))
+    print(len(sequences_train))
+    with open(output_train_file_path, 'wb') as output_file:
+        np.save(output_file, np.asarray(sequences_train))
+    # Convert test text to word-embedding
+    with open(texts_test_file_path, 'rb') as texts_test_file:
+        texts_test = pickle.load(texts_test_file)
+    sequences_test = tokenizer.texts_to_sequences(texts_test)
+    sequences_test = pad_sequences(sequences_test, maxlen=max_sequence_length)
+    # Save test sequences
+    print(len(sequences_test))
+    with open(output_test_file_path, 'wb') as output_file:
+        np.save(output_file, np.asarray(sequences_test))
+
+    # Generate file path
+    word2index_dict_file_path = os.path.join(
+        os.path.pardir, "data", "word2vec_dict")
+    index2vec_mat_file_path = os.path.join(
+        os.path.pardir, "data", "word2vec_matrix")
+    word_embedding_mat_file_path = os.path.join(
+        os.path.pardir, "data", "word-embedding_matrix")
+    # Read data from files
+    with open(word2index_dict_file_path, 'rb') as word2index_dict_file:
+        word2index_dict = pickle.load(word2index_dict_file)
+    with open(index2vec_mat_file_path, 'rb') as index2vec_mat_file:
+        index2vec_matrix = np.load(index2vec_mat_file)
+    # Convert word2vec matrix to word-embedding matrix
+    word_embedding_matrix_shape = (
+        len(tokenizer.word_index) + 1, index2vec_matrix.shape[1])  # +1 for not exist word
+    word_embedding_matrix = np.zeros(word_embedding_matrix_shape)
+    for word, index in tokenizer.word_index.items():
+        word2vec_index = word2index_dict.get(word)
+        if word2vec_index is not None:
+            # Vector of words not found in word2vec will be all-zeros.
+            word_embedding_matrix[index] = index2vec_matrix[word2vec_index]
+    # Save word-embedding matrix
+    print(word_embedding_matrix.shape)
+    with open(word_embedding_mat_file_path, 'wb') as word_embedding_mat_file:
+        np.save(word_embedding_mat_file, word_embedding_matrix)
 
 
 def classification(file_name_suffix):
@@ -237,6 +303,13 @@ def verify_word2vec():
 
 
 if __name__ == "__main__":
+    # Demo data
+    file_name_suffix = "demo"
+    collect_data("sinanews.train", file_name_suffix)
+    verify_data(file_name_suffix)
+    # Demo label
+    classification(file_name_suffix)
+    regression(file_name_suffix)
     # Train data
     file_name_suffix = "train"
     collect_data("sinanews.train", file_name_suffix)
@@ -251,9 +324,10 @@ if __name__ == "__main__":
     # Test label
     classification(file_name_suffix)
     regression(file_name_suffix)
+    # word2vec data
+    collect_word2vec()
+    verify_word2vec()
     # Train texts
     bags_of_words()
     tf_idf()
-    # word2vec
-    collect_word2vec()
-    verify_word2vec()
+    word_embedding()
