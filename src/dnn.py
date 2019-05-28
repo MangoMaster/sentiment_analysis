3bfefac41c90cfc124531e7e708afc11ddef0bfe
MangoMaster <weijd17@mails.tsinghua.edu.cn>
2019-05-28 22:33:46 +0800

    Modify dnn

--- src/dnn.py
+++ src/dnn.py
@@ -1,64 +1,78 @@
 import os
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.optimizers import SGD, Adam
+from keras.layers import Dense, Dropout
 from utilnn import Fscore, coef
 
 
-def dnn(texts_prefix, labels_prefix):
+def load_data(texts_prefix, labels_prefix):
     """
-    Fully connected neural network.
+    @param texts_prefix: 'bags-of-words' or 'tf-idf'
+    @param labels_prefix: 'classfication' or 'regression'
+    @return: (inputs_train, outputs_train, inputs_test, outputs_test)
     """
     # Generate file path
-    texts_train_file_path = os.path.join(
+    inputs_train_file_path = os.path.join(
         os.path.pardir, "data", texts_prefix + "_train")
-    labels_train_file_path = os.path.join(
+    outputs_train_file_path = os.path.join(
         os.path.pardir, "data", labels_prefix + "_train")
-    texts_test_file_path = os.path.join(
+    inputs_test_file_path = os.path.join(
         os.path.pardir, "data", texts_prefix + "_test")
-    labels_test_file_path = os.path.join(
+    outputs_test_file_path = os.path.join(
         os.path.pardir, "data", "regression" + "_test")  # test label always use regression
     # Get data
-    with open(texts_train_file_path, 'rb') as texts_train_file:
-        texts_train = np.load(texts_train_file)
-    with open(labels_train_file_path, 'rb') as labels_train_file:
-        labels_train = np.load(labels_train_file)
-    with open(texts_test_file_path, 'rb') as texts_test_file:
-        texts_test = np.load(texts_test_file)
-    with open(labels_test_file_path, 'rb') as labels_test_file:
-        labels_test = np.load(labels_test_file)
+    with open(inputs_train_file_path, 'rb') as inputs_train_file:
+        inputs_train = np.load(inputs_train_file)
+    with open(outputs_train_file_path, 'rb') as outputs_train_file:
+        outputs_train = np.load(outputs_train_file)
+    with open(inputs_test_file_path, 'rb') as inputs_test_file:
+        inputs_test = np.load(inputs_test_file)
+    with open(outputs_test_file_path, 'rb') as outputs_test_file:
+        outputs_test = np.load(outputs_test_file)
+    return (inputs_train, outputs_train, inputs_test, outputs_test)
+
+
+def dnn(inputs_train, outputs_train, inputs_test, outputs_test, loss):
+    """
+    Fully connected neural network.
+    @param loss: 'categorical_crossentropy' or 'mean_squared_error'
+    """
     # Build DNN model
     model = Sequential()
-    model.add(Dense(64, activation='relu', input_dim=texts_train.shape[1]))
+    model.add(Dense(64, activation='relu', input_dim=inputs_train.shape[1]))
     model.add(Dropout(0.5))
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(labels_train.shape[1], activation='softmax'))
-    # loss
-    if labels_prefix == "classification":
-        loss = 'categorical_crossentropy'
-    elif labels_prefix == "regression":
-        loss = 'mean_squared_error'
-    else:
-        raise ValueError("labels_prefix doesn't exist.")
-    # optimizer
-    # optimizer = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    optimizer = Adam()
+    model.add(Dense(outputs_train.shape[1], activation='softmax'))
     # compile
-    model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy', coef])
+    model.compile(loss=loss, optimizer='adam', metrics=['accuracy', coef])
     # train
     fscore = Fscore()
-    model.fit(texts_train, labels_train, epochs=60, batch_size=256,
-              validation_data=(texts_test, labels_test), callbacks=[fscore])
+    model.fit(inputs_train, outputs_train, epochs=60, batch_size=256,
+              validation_data=(inputs_test, outputs_test), callbacks=[fscore])
     # evaluate
-    score = model.evaluate(texts_test, labels_test, batch_size=256)
+    score = model.evaluate(inputs_test, outputs_test, batch_size=256)
     print("Eval: loss: %.4f - acc - %.4f - fscore: %.4f - coef: %.4f" %
           (score[0], score[1], fscore.get_data(), score[2]))
 
 
 if __name__ == "__main__":
-    dnn("bags-of-words", "classification")
-    dnn("bags-of-words", "regression")
-    dnn("tf-idf", "classification")
-    dnn("tf-idf", "regression")
+    inputs_train, outputs_train, inputs_test, outputs_test = \
+        load_data("bags-of-words", "classification")
+    dnn(inputs_train, outputs_train, inputs_test, outputs_test,
+        loss='categorical_crossentropy')
+
+    inputs_train, outputs_train, inputs_test, outputs_test = \
+        load_data("bags-of-words", "regression")
+    dnn(inputs_train, outputs_train, inputs_test, outputs_test,
+        loss='mean_squared_error')
+
+    inputs_train, outputs_train, inputs_test, outputs_test = \
+        load_data("tf-idf", "classification")
+    dnn(inputs_train, outputs_train, inputs_test, outputs_test,
+        loss='categorical_crossentropy')
+
+    inputs_train, outputs_train, inputs_test, outputs_test = \
+        load_data("tf-idf", "regression")
+    dnn(inputs_train, outputs_train, inputs_test, outputs_test,
+        loss='mean_squared_error')
