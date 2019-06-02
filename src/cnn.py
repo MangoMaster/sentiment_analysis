bacb96a19fc7c1c2c9589344438a303bc0b76a26
MangoMaster <weijd17@mails.tsinghua.edu.cn>
2019-06-02 12:01:47 +0800

    Add training for embedding layer

--- src/cnn.py
+++ src/cnn.py
@@ -1,4 +1,5 @@
 import os
+import functools
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Embedding, Conv1D, MaxPooling1D, Flatten, Dense, Dropout
@@ -7,7 +8,6 @@ from utilnn import Fscore, coef
 
 def load_data(labels_prefix):
     """
-    @param texts_prefix: 'bags-of-words' or 'tf-idf'
     @param labels_prefix: 'classfication' or 'regression'
     @return: (inputs_train, outputs_train, inputs_test, outputs_test)
     """
@@ -33,18 +33,30 @@ def load_data(labels_prefix):
     return (inputs_train, outputs_train, inputs_test, outputs_test)
 
 
-def cnn(inputs_train, outputs_train, inputs_test, outputs_test, loss):
+def cnn(inputs_train, outputs_train, inputs_test, outputs_test, loss, train_embedding):
     """
     Convolutional neural network.
     @param loss: 'categorical_crossentropy' or 'mean_squared_error'
+    @param train_embedding: 0 - initialize with word_embedding_matrix, trainable=False
+                            1 - initialize with word_embedding_matrix, trainable=True
+                            2 - initialize with random matrix, trainable=True
     """
     word_embedding_matrix_file_path = os.path.join(
         os.path.pardir, "data", "word-embedding_matrix")
     with open(word_embedding_matrix_file_path, 'rb') as word_embedding_matrix_file:
         word_embedding_matrix = np.load(word_embedding_matrix_file)
     # Build CNN model
-    embedding_layer = Embedding(word_embedding_matrix.shape[0], word_embedding_matrix.shape[1], weights=[word_embedding_matrix],
-                                input_length=inputs_train.shape[1], trainable=False)
+    if train_embedding == 0:
+        embedding_layer = Embedding(word_embedding_matrix.shape[0], word_embedding_matrix.shape[1], weights=[word_embedding_matrix],
+                                    input_length=inputs_train.shape[1], trainable=False)
+    elif train_embedding == 1:
+        embedding_layer = Embedding(word_embedding_matrix.shape[0], word_embedding_matrix.shape[1], weights=[word_embedding_matrix],
+                                    input_length=inputs_train.shape[1], trainable=True)
+    elif train_embedding == 2:
+        embedding_layer = Embedding(word_embedding_matrix.shape[0], word_embedding_matrix.shape[1],
+                                    input_length=inputs_train.shape[1], trainable=True)
+    else:
+        raise ValueError("train_embedding should be 0 or 1 or 2.")
     model = Sequential()
     model.add(embedding_layer)
     model.add(Conv1D(128, 3, activation='relu'))
@@ -73,13 +85,25 @@ def cnn(inputs_train, outputs_train, inputs_test, outputs_test, loss):
           (score[0], score[1], fscore.get_data(), score[2]))
 
 
+cnn_static = functools.partial(cnn, train_embedding=0)
+cnn_non_static = functools.partial(cnn, train_embedding=1)
+cnn_rand = functools.partial(cnn, train_embedding=2)
+
 if __name__ == "__main__":
     inputs_train, outputs_train, inputs_test, outputs_test = \
         load_data("classification")
-    cnn(inputs_train, outputs_train, inputs_test, outputs_test,
-        loss='categorical_crossentropy')
+    cnn_static(inputs_train, outputs_train, inputs_test, outputs_test,
+               loss='categorical_crossentropy')
+    cnn_non_static(inputs_train, outputs_train, inputs_test, outputs_test,
+                   loss='categorical_crossentropy')
+    cnn_rand(inputs_train, outputs_train, inputs_test, outputs_test,
+             loss='categorical_crossentropy')
 
     inputs_train, outputs_train, inputs_test, outputs_test = \
         load_data("regression")
-    cnn(inputs_train, outputs_train, inputs_test, outputs_test,
-        loss='mean_squared_error')
+    cnn_static(inputs_train, outputs_train, inputs_test, outputs_test,
+               loss='mean_squared_error')
+    cnn_non_static(inputs_train, outputs_train, inputs_test, outputs_test,
+                   loss='mean_squared_error')
+    cnn_rand(inputs_train, outputs_train, inputs_test, outputs_test,
+             loss='mean_squared_error')
